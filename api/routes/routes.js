0caa6fcf37bf63ba051e12d10983f350be11dd8d
Morgan Gicheha <morgangicheha4@gmail.com>
2022-12-10 10:49:53 +0300

    working version before socket implemetation

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -8,27 +8,35 @@ router.get("/", async (req, res) => {
     let userID = uuid();
 
     setUID(userID);
-
     let image = await generateQr(userID);
 
-    res.send(userID);
+    res.status(200).send({
+        succesfull: false,
+        randomUserID: userID,
+        base64ImageQR: image,
+    })
 });
 
 router.get("/user/:UserID", async (req, res) => {
     // todo: update firebase with uid
     let userID = req.params.UserID
-
-    if(!userID){
+    if (!userID) {
         console.log("user id null")
         res.send("user id null")
     }
-
-
     // res.send(req.params.UserID)
 
-    let snapshotRes =  await   realTimeSnapshots(userID)
-
+    let snapshotRes = await realTimeSnapshots(userID)
     res.send(snapshotRes)
+});
+
+
+
+router.get('*', function (req, res) {
+    res.status(404).send({
+        succesfull: true,
+        reason: "path does not exist",
+    })
 });
 
 module.exports = router;
